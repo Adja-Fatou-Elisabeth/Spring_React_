24e380dde1bfb9f23193e7e47f7ddc905b053052
Adja-Fatou-Elisabeth <adjafatouelisabeth@gmail.com>
2019-10-04 12:39:24 +0000

    Creation du projet

---
+++ src/main/java/com/adja/SpringReact/SpringReactNativeApplication.java
@@ -0,0 +1,13 @@
+package com.adja.SpringReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adja/SpringReact/SpringReactNativeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adja.SpringReact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringReactNativeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
